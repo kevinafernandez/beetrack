739fafd271783af3c0d5924b54722bdd54a166a3
Kevin Fernandez <kevinafernandezm@gmail.com>
2019-08-26 19:30:52 -0400

    Beetrack | Delete redudant column in Waypoint Table via migration

--- db/migrate/20190825005619_create_waypoints.rb
+++ db/migrate/20190825005619_create_waypoints.rb
@@ -4,7 +4,6 @@ class CreateWaypoints < ActiveRecord::Migration[5.0]
       t.decimal :latitude, precision: 10, scale: 6
       t.decimal :longitude, precision: 10, scale: 6
       t.datetime :sent_at
-      t.string :vehicle_identifier
       t.references :vehicle, null: false, foreign_key: true
 
       t.timestamps
